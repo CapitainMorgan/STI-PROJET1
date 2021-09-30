5583bbbe14003b9d1b9904d98ebadffaadcbc4db
CapitainMorgan <matthieugodi@vtx.ch>
2021-09-30 13:36:13 +0200

    remove com

--- src/php/404.php
+++ src/php/404.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Page de redirection de page inexistante
+ * 
  * Created by PhpStorm.
- * User: Axel Vallon, Lev Pozniakoff, Maude Issolah, Matthieu Godi
- * Date: 01.01.2020
+ * User: 
+ * Date: 
  */
 ?>
 <h1>Erreur 404</h1>
