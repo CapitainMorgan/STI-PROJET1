4526d79afcd8315105147d57d27a751e8d42f9d6
issolahma <22448610+issolahma@users.noreply.github.com>
2021-09-30 14:06:31 +0200

    update index.php

--- src/php/index.php
+++ src/php/index.php
@@ -6,7 +6,7 @@ session_start();
 <!doctype html>
 <html lang="fr" onkeydown="pressKey(event)" >
 <head>
-    <title>StudiJob</title>
+    <title>STI - Projet1</title>
     <link href="../../resources/css/default.css" rel="Stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
@@ -24,45 +24,7 @@ session_start();
     </header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="?">StudiJob <?php if((isset($_SESSION['typeCompte']) && $_SESSION['typeCompte'] == "moderateur") || (isset($_GET['typeConnexion']) && $_GET['typeConnexion'] == "mod")) echo 'Moderateur' ?></a>
-            <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                <ul class="navbar-nav mb-2 mb-lg-0 right">
-                    <li class="nav-item dropdown  ">
-                        <?php
-                        /*if(isset($_SESSION['pseudo']) && isset($_SESSION['typeCompte'])) {
-                            echo '<a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                ' . $_SESSION['pseudo'] . '</a>';
-                            if ($_SESSION['typeCompte'] == "etudiant")
-                                echo '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="?page=monCompte">Mon compte</a></li>
-                                    <li><a class="dropdown-item" href="?page=accueil">Offre d\'emploi</a></li>
-                                    <li><a class="dropdown-item" href="?page=listePostulation">Mes postulations</a></li>
-                                    <li><a class="dropdown-item" href="?page=messagerie">Messageries</a></li>
-                                    <li><a class="dropdown-item" href="?page=deconnexion">Se déconnecter</a></li>
-                                </ul>';
-                            elseif ($_SESSION['typeCompte'] == "employeur")
-                                echo '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="?page=monCompte">Mon compte</a></li>
-                                    <li><a class="dropdown-item" href="?page=accueil">Postulations</a></li>
-                                    <li><a class="dropdown-item" href="?page=listeOffreEmploi">Mes offres d\'emploi</a></li>
-                                    <li><a class="dropdown-item" href="?page=offreEmploiFrom">Créer une nouvelle offre</a></li>
-                                    <li><a class="dropdown-item" href="?page=messagerie">Messageries</a></li>
-                                    <li><a class="dropdown-item" href="?page=deconnexion">Se déconnecter</a></li>
-                                </ul>';
-                            elseif ($_SESSION['typeCompte'] == "moderateur")
-                                echo '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="?page=monCompte">Mon compte</a></li>
-                                    <li><a class="dropdown-item" href="?page=accueil">Offre d\'emploi</a></li>
-                                    <li><a class="dropdown-item" href="?page=signalement">Signalements</a></li>
-                                    <li><a class="dropdown-item" href="?page=tag">Tags</a></li>
-                                    <li><a class="dropdown-item" href="?page=deconnexion">Se déconnecter</a></li>
-                                </ul>';
-                        }*/
-                        ?>
-                    </li>
-                </ul>
-            </div>
-        </div>
+            <a class="navbar-brand" href="?">STI - Projet1 <?php if((isset($_SESSION['typeCompte']) && $_SESSION['typeCompte'] == "moderateur") || (isset($_GET['typeConnexion']) && $_GET['typeConnexion'] == "mod")) echo 'Moderateur' ?></a>
     </nav>
     <section>
         <?php
@@ -82,7 +44,7 @@ session_start();
     </section>
     <footer  class="fixed-bottom">
         <div class="container-fluid">
-            <p class="text-center">&copy StudiJob</p>
+            <p class="text-center">Godi - Issolah</p>
         </div>
     </footer>
 
