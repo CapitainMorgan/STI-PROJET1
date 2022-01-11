2cb01c88e194ea7a4fa6ee929bf48458f505b1b8
WenesLimem <wanous19@gmail.com>
2022-01-11 10:13:29 +0100

    [DONE] add hidden recaptcha

--- app/src/php/connexion.php
+++ app/src/php/connexion.php
@@ -6,21 +6,29 @@
  * Date: 13.10.2021
  */
 ?>
-
+<script src="https://www.google.com/recaptcha/api.js"></script>
 <div class="wrapper fadeInDown">
   <div id="formContent">
     <!-- Tabs Titles -->
-      <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
     <!-- Login Form -->
     <form action="validation.php" method="post">
 
       <input type="text" id="username" class="fadeIn second" name="username" placeholder="Nom utilisateur" required>
       <input type="password" id="password" class="fadeIn third" name="password" placeholder="Mot de passe" required>
-        <div class="g-recaptcha" style="padding-left: 70px" data-sitekey="<?php echo'6Ldl6OwdAAAAADbhthvzsiiOmnflItytfLJUzugC';?>"></div>
         <input type="submit" class="fadeIn fourth" name="login" value="Log In">
-
+        <input type="hidden" name="recaptcha_response" value="" id="recaptchaResponse">
 
     </form>
 
   </div>
+    <script>
+        grecaptcha.ready(function(){
+            grecaptcha.execute('6LepLPkdAAAAAPxabk3uO1p9b_HptCAJNmbU0Okr', { action:'submit'}).then(function (token){
+                var recaptchaResponse=document.getElementById('recaptchaResponse');
+                recaptchaResponse.value=token;
+            })
+        })
+    </script>
 </div>
+
--- app/src/php/validation.php
+++ app/src/php/validation.php
@@ -11,7 +11,6 @@ use ReCaptcha\ReCaptcha;
 session_start();
 include 'classes/InputValidation.php';
 include("classes/DB.php");
-require_once('classes/recaptchalib.php');
 
 if (!empty($_POST["login"])) {
 
@@ -20,7 +19,16 @@ if (!empty($_POST["login"])) {
     $db = new DB();
 
     $login_result = $db->loginValidation($username);
-
+    $recaptcha_url="https://www.google.com/recaptcha/api/siteverify";
+    $secret_key="6LepLPkdAAAAAGryCm-BxmTSuYMkv0N5_MrH_Xqm";
+    $recaptcha_response=$_POST['recaptcha_response'];
+    $get_recaptcha_response=file_get_contents($recaptcha_url . '?secret='.$secret_key.'&response='.$recaptcha_response);
+    $response_json=json_decode($get_recaptcha_response);
+    if($response_json->success == true && $response_json->score>=0.5 && $response_json->action=='submit') {
+        $_SESSION['secured'] = "Secured";
+    }else {
+        header("Location:index.php?error=Recaptcha");
+    }
     if (!empty($login_result)) {
         if(password_verify($password, $login_result[0]['MotDePasse']))
         {    
@@ -29,16 +37,6 @@ if (!empty($_POST["login"])) {
             header("Location:index.php?page=home");
             exit;
         }    
-    }
-
-    if (isset($_POST['g-recaptcha-response'])){
-        require('component/recaptcha/src/autoload.php');
-        $recaptcha = new ReCaptcha('6Ldl6OwdAAAAADbhthvzsiiOmnflItytfLJUzugC');
-        $resp = $recaptcha->verify($_POST['g-recaptcha-response'],$_SERVER['REMOTE_ADDR']);
-        if (!$resp->isSuccess()){
-            $output = json_encode(array('type' => 'error', 'text' => '<b>Captcha</b> Validation Required!'));
-            die($output);
-        }
     }
 }
 header("Location:index.php?error=1");
